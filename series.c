1247043f76e694e29ed236147e6be2448abb2cf3
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2011-11-29 01:23:47 +0100

    Adds test()

--- series.c
+++ series.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <wchar.h>
 
-const char *program_name;
+const char   *program_name;
+const wchar_t bar_characters[] = L" ▁▂▃▄▅▆▇";
 
 void error(const char *msg)
 {
@@ -15,12 +17,22 @@ void die(const char *msg, int status)
 	exit(status);
 }
 
+/*******************************************************************/
+void test(void)
+{
+	printf("wcslen(bar_characters) = %zi\n", wcslen(bar_characters));
+	printf("bar_characters = %ls\n", bar_characters);
+}
+/*******************************************************************/
+
 int main(int argc, char *argv[])
 {
 	program_name = argv[0];
 	if (argc > 1)
 		die("fuck you!", EXIT_FAILURE);
 	setlocale(LC_ALL, "");
+
+	test();
 
 	return EXIT_SUCCESS;
 }
