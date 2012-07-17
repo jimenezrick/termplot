ffa4d46dd99f84d78ae9551e302d93ba241908ef
Ricardo Catalinas Jiménez <jimenezrick@gmail.com>
2012-07-17 21:56:06 +0200

    Minimal Haskell version

--- termplot.hs
+++ termplot.hs
@@ -1,30 +1,37 @@
 module Main where
 
-import Graphics.Vty
-import Control.Concurrent
+import System.Environment
+import Data.List.Split
+{-import Graphics.Vty-}
 
-main2 :: IO ()
-main2 = do
-    vty <- mkVty
-    update vty $ pic_for_image $ string def_attr " ▁▂▃▄▅▆▇"
-    {-refresh vty-}
-    threadDelay $ 1000 * 1000
-    shutdown vty
-    putStrLn "End"
+{-import Control.Concurrent-}
+{-main2 :: IO ()-}
+{-main2 = do-}
+    {-vty <- mkVty-}
+    {-update vty $ pic_for_image $ string def_attr " ▁▂▃▄▅▆▇"-}
+    {-[>refresh vty<]-}
+    {-threadDelay $ 1000 * 1000-}
+    {-shutdown vty-}
+    {-putStrLn "End"-}
 
-
-
-
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+      [] -> return ()
+      _  -> do
+          let seq = map read $ concat $ map (splitOn ",") args :: [Double]
+          putStrLn $ getBars seq
 
 bars :: String
 bars = " ▁▂▃▄▅▆▇█"
 
-getBar :: Double -> Double -> Char
-getBar max n =
-    let len = length bars
-        wid = fromIntegral len / (max + 1)
-        idx = round $ wid * n
-    in bars !! (min idx (len - 1))
+getBar :: Double -> Double -> Double -> Char
+getBar min max n =
+    let len = fromIntegral $ length bars
+        wid = (max - min) / (len - 1)
+        idx = round $ (n - min) / wid
+    in bars !! idx
 
 getBars :: [Double] -> String
-getBars l = map (getBar $ maximum l) l
+getBars l = map (getBar (minimum l) (maximum l)) l
